7e2509ace6594f2706d6867a92098225297357d6
Chaten Mundle <chetanmundle7875@gmail.com>
2024-11-29 08:19:14 +0530

    Login Dto

--- App.Core/obj/Debug/net8.0/App.Core.AssemblyInfo.cs
+++ App.Core/obj/Debug/net8.0/App.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e9c98615e026df2515e83eed07b0d54d251fd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c88610a2823237aa238087529eb0ae8f18570e")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
