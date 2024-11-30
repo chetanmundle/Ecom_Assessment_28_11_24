c207607d321691e09f2491b760f406dddb1466cc
Chaten Mundle <chetanmundle7875@gmail.com>
2024-12-01 00:14:22 +0530

    logout

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, inject, OnDestroy } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { UserWithoutPassDto } from '../../../core/models/interface/User/UserWithoutPass';
 import { UserService } from '../../../core/services/UserService/user.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -16,8 +17,9 @@ export class NavBarComponent implements OnDestroy {
   subscriptions: Subscription = new Subscription();
 
   private userService = inject(UserService);
+  private router = inject(Router);
 
-  constructor(private el: ElementRef) {
+  constructor() {
     const sub = this.userService.loggedUser$.subscribe({
       next: (user: UserWithoutPassDto) => {
         this.loggedUser = user;
@@ -26,9 +28,23 @@ export class NavBarComponent implements OnDestroy {
 
     this.subscriptions.add(sub);
   }
-
-  
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
+  }
+
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  onClickLogOut() {
+    localStorage.removeItem('accessToken');
+    this.userService.resetLoggedUser();
+    this.router.navigateByUrl('/auth/Login');
   }
 }
